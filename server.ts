a635a43f6a5c3377d785dd336c6990a9641161bb
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-03-05 01:21:25 +0545

    Set Access-Control-Allow-Origin header

--- server.ts
+++ server.ts
@@ -11,6 +11,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const isProduction = process.env.NODE_ENV === "production";
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    isProduction
+      ? "https://abhishekram-404-writepad.netlify.app"
+      : "http://localhost:3000"
+  );
+  next();
+});
 cors({
   origin: isProduction
     ? "https://abhishekram-404-writepad.netlify.app"
