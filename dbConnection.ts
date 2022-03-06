8d1507915b1c5bfc8e57c531e22cd0cccc1b9133
Keshav Kishor Ram <55845306+abhishekram404@users.noreply.github.com>
2022-03-06 15:14:36 +0545

    Add default URI

--- dbConnection.ts
+++ dbConnection.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  process.env.MONGO_URI,
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/writepad",
   (err) => {
     if (err) {
       return console.log(err.message);
